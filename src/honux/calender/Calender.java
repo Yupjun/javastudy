2b52e64240aa9a876b0ac564424d82e75b582c54
junyaup.Kim <yaup22c@naver.com>
2017-09-06 15:50:04 +0900

    gugudan

--- src/honux/calender/Calender.java
+++ src/honux/calender/Calender.java
@@ -3,8 +3,17 @@ package honux.calender;
 public class Calender {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Calender");
-
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		for (int j = 0; j < 4; j++){
+			for (int k = 1; k < 8; k++){
+				if (k==7){
+					System.out.println(j*7 + k);
+				} else {
+					System.out.print(j*7 + k + " ");
+				}
+				
+			}
+		}
 	}
-
 }
